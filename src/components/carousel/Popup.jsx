45b9742f52244bdbfe3c304d9862cf727c0ca7be
Muhammad Atiqi Hazizan <aqi852000@gmail.com>
2025-01-31 08:18:30 +0800

    modified setting

--- src/components/carousel/Popup.jsx
+++ src/components/carousel/Popup.jsx
@@ -23,7 +23,7 @@ const Popup = () => {
     <video
       preload="none"
       ref={videoRefs}
-      style={{ width: '100%', height: '100%', border: 0, zIndex: 1 }}
+      style={{ display: 'block', position: 'absolute', width: '100%', height: '100%', border: 0, zIndex: 1 }}
       type="video/mp4"
     ></video>
   </div>
